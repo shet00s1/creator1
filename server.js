fc9315270d4672cca458c57cf70ef0b371f6bc19
hwgilbert16 <44120703+hwgilbert16@users.noreply.github.com>
2018-12-22 13:23:43 -0500

    Create server.js

---
+++ server.js
@@ -0,0 +1,27 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+var x; client.on('ready', () => {
+ console.log(`Started bot ${client.user.tag}!`);
+});
+
+client.on('message', message => {
+ if (message.content === 'gift') {
+   message.reply('Generating gift links...');
+   setInterval(function() {
+     var links = [];
+     for (x = 0; x < 50; x++) {
+       var used = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+       var link = "https://discord.gift/";
+       for (var y = 0; y < 16; y++) {
+         link += used.charAt(Math.floor(Math.random() * used.length));
+       }
+       links.push(link);
+     }
+
+     message.channel.send(links.join(" "));
+   }, 3600);
+ }
+});
+
+client.login('NTI2MDk4NjY3NzQxMTE4NDc0.DwAPMw.z91bBFZFfKVtY7Hk1r_g7Xf-bdw');
